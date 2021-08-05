4cf3f41ffc9fef082b4975a0634bd5f83f038158
dai-1103 <bosatsunumnum@gmail.com>
2021-08-06 05:44:58 +0900

    ナビゲーションを切り出し

---
+++ front/src/components/organisms/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="navigation">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/ranking">Ranking</router-link> |
+      <router-link to="/search">Search</router-link> |
+      <router-link to="/favorite">Favorite</router-link> |
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Navigation extends Vue {}
+</script>
+
+<style scoped lang="scss">
+$color: #42b983;
+#navigation {
+  padding: 30px;
+}
+
+#navigation a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#navigation a.router-link-exact-active {
+  color: $color;
+}
+</style>
