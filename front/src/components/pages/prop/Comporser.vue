89c4a75b36f740dd7cf2091bb80e0442cf9b0ec1
dai-1103 <bosatsunumnum@gmail.com>
2021-08-07 07:58:13 +0900

    詳細ページのベースを配置

---
+++ front/src/components/pages/prop/Comporser.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Template extends Vue {}
+</script>
+
+<style scoped lang="scss">
+</style>
