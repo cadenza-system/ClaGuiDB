90c59c03dea76bf97f9adea767adc09c87ed7081
dai-1103 <bosatsunumnum@gmail.com>
2021-08-07 07:58:43 +0900

    pagesディレクトリ整理

---
+++ front/src/components/pages/main/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <Header />
+    <VideoLink />
+    <VideoLink />
+    <Navigation />
+  </div>
+</template>
+
+<script scoped lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Header from "../organisms/Header.vue";
+import VideoLink from "../organisms/VideoLink.vue"
+import Navigation from "../organisms/Navigation.vue";
+
+@Component({
+  components: {
+    Header,
+    VideoLink,
+    Navigation
+  },
+})
+export default class Home extends Vue {}
+</script>
---
+++ front/src/components/pages/main/Ranking.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ranking">
+    <Header />
+    <h2>RANKING</h2>
+    <Navigation />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Header from "../organisms/Header.vue";
+import Navigation from "../organisms/Navigation.vue";
+
+@Component({
+  components: {
+    Header,
+    Navigation
+  },
+})
+export default class Ranking extends Vue {}
+</script>
