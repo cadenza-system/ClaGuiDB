39cc70452d9834aa9916867c08f620bcf13d83fb
dai-1103 <bosatsunumnum@gmail.com>
2021-08-07 08:25:26 +0900

    videoLinkをリスト化

---
+++ front/src/components/organisms/VideoLinkList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="video-link-list">
+    <VideoLink />
+    <VideoLink />
+    <VideoLink />
+    <VideoLink />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import VideoLink from "./VideoLink.vue";
+
+@Component({
+  components: {
+    VideoLink
+  }
+})
+export default class VideoLinkList extends Vue {}
+</script>
+
+<style scoped lang="scss">
+</style>
