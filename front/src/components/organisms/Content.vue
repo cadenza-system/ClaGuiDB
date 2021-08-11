833e697fc921528dd3375ad42dc223297d422b5b
dai-1103 <bosatsunumnum@gmail.com>
2021-08-11 19:58:03 +0900

    ボディ部の余白を追加

---
+++ front/src/components/organisms/Content.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="content">
+  <slot></slot>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class Content extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.content{
+  padding-top: 30vw;
+  padding-bottom: 10vw;
+}
+</style>
