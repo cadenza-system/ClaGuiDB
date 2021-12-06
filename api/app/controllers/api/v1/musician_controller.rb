d22c4d89991b7980985366951a7ef1caf51240c6
dai-1103 <bosatsunumnum@gmail.com>
2021-12-06 13:36:41 +0900

    musicianのapi実装

---
+++ api/app/controllers/api/v1/musician_controller.rb
@@ -0,0 +1,22 @@
+module Api
+    module V1
+        class MusicianController < ApplicationController
+            before_action :set_musician, only: [:show, :update, :destroy]
+            # GET /musician
+            def index
+                musicians = Musician.all
+
+                render json: { status: 'SUCCESS', message: 'Loaded musicians', data: musicians}
+            end
+
+            # GET /musician/:id
+            def show
+                render json: { status: 'SUCCESS', message: 'Loaded the musician', data: @musician }
+            end
+
+            def set_musician
+                @musician = Musician.find(params[:id])
+            end
+        end
+    end
+end
---
+++ api/db/schema.rb
@@ -0,0 +1,98 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_12_06_023744) do
+
+  create_table "CD", primary_key: "ID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "J_TITLE"
+    t.string "E_TITLE"
+    t.integer "RELEASE_YEAR"
+    t.string "LABEL"
+    t.string "LINK", limit: 755, null: false
+    t.string "ASIN", null: false
+    t.integer "DEL_FLG", null: false
+  end
+
+  create_table "CD_PLAYERS", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "CD_ID", null: false
+    t.integer "MUSICIAN_ID", null: false
+    t.integer "INDEX", null: false
+  end
+
+  create_table "CD_WORKS", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.integer "CD_ID", null: false
+    t.integer "WORKS_ID"
+    t.string "WORKS_TITLE", null: false
+    t.string "WORKS_TITLE_SUB"
+    t.integer "INDEX", null: false
+  end
+
+  create_table "MOVIE", primary_key: ["ID", "WORKS_ID"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "ID", null: false
+    t.integer "WORKS_ID", null: false
+    t.string "J_SUB_TITLE", null: false
+    t.string "E_SUB_TITLE"
+    t.string "URL", null: false
+    t.integer "TYPE", null: false
+    t.integer "ARRANGER"
+    t.integer "ORDER_NO"
+    t.integer "DEL_FLG", default: 0, null: false
+  end
+
+  create_table "MUSICIAN", primary_key: "ID", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "J_NAME", null: false
+    t.string "E_NAME", null: false
+    t.string "LAST_NAME", null: false
+    t.string "FROM_YEAR", limit: 5
+    t.string "TO_YEAR", limit: 5
+    t.integer "COMP_FLG", default: 0, null: false
+    t.integer "PLAY_FLG", default: 0, null: false
+    t.integer "DEL_FLG", default: 0, null: false
+  end
+
+  create_table "MUSICIAN_COUNTRY", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "MUSICIAN_ID", null: false
+    t.integer "COUNTRY_ID", null: false
+  end
+
+  create_table "M_AREA", primary_key: "ID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "NAME", null: false
+  end
+
+  create_table "M_COUNTRY", primary_key: "ID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "NAME", null: false
+    t.string "AREA", null: false
+  end
+
+  create_table "M_LABEL", primary_key: "ID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "NAME", null: false
+  end
+
+  create_table "M_TYPE", primary_key: "ID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "NAME", null: false
+  end
+
+  create_table "WORKS", primary_key: "ID", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "J_TITLE"
+    t.string "E_TITLE"
+    t.string "OUPS"
+    t.string "COMPOSER"
+    t.string "SUB_COMPOSER"
+    t.integer "DEL_FLG", default: 0, null: false
+  end
+
+  create_table "musicians", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
